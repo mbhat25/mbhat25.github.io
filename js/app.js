9239ec093e2b85da23887b5c1f8c0cd866d83448
mbhat25 <mbhat@barneys.com>
2016-07-07 18:43:07 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+$(function(){
+  $('#responsive-nav').click(function(){
+    $(this).parent().toggleClass('slideDown');
+  })
+});
+
+//togleClass adds a class on click and then remove the class on subsequent click
